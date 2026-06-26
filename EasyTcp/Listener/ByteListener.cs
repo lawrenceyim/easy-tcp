e0a08dc44b144011cedefa5c650f999d2d45c8d7
lawrenceyim <lawrence.yim.dev@gmail.com>
2026-06-25 20:54:35 -0700

    Fixed issue with json listener not relaying connected and disconnected event.

---
+++ EasyTcp/Listener/ByteListener.cs
@@ -0,0 +1,61 @@
+using System.Net;
+using System.Net.Sockets;
+
+namespace EasyTcp;
+
+public class ByteListener {
+    public event Action<byte[]> BytesReceived;
+    public event Action<string> ClientConnected;
+    public event Action<string> ClientDisconnected;
+
+    private readonly TcpListener _listener;
+    private CancellationTokenSource _cancelTokenSource = new();
+
+    public ByteListener(string ipAddress, int port) {
+        _listener = new TcpListener(IPAddress.Parse(ipAddress), port);
+    }
+
+    public void Start() {
+        _listener.Start();
+        _cancelTokenSource = new CancellationTokenSource();
+
+        while (!_cancelTokenSource.IsCancellationRequested) {
+            TcpClient client = _listener.AcceptTcpClient();
+            Task.Run(() => { _ = _HandleClient(client, _cancelTokenSource.Token); }, _cancelTokenSource.Token);
+        }
+    }
+
+    public void Stop() {
+        _cancelTokenSource.Cancel();
+        _listener.Stop();
+    }
+
+    private Task _HandleClient(TcpClient client, CancellationToken token) {
+        NetworkStream stream = client.GetStream();
+        byte[] buffer = new byte[1024];
+        string endPoint = $"{client.Client.RemoteEndPoint}";
+        ClientConnected?.Invoke(endPoint);
+
+        try {
+            // Console.WriteLine($"{client.Client.RemoteEndPoint} try");
+            while (!token.IsCancellationRequested) {
+                stream.ReadExactly(buffer, 0, 4);
+                int length = BufferReader.ReadInt(buffer, 0);
+                stream.ReadExactly(buffer, 4, length);
+                BytesReceived?.Invoke(buffer[4..(length + 4)]);
+            }
+        }
+        catch (EndOfStreamException) {
+            // Console.WriteLine($"{endPoint} stream ended");
+        }
+        catch (Exception e) {
+            // Console.WriteLine($"{endPoint} catch with exception: {e}");
+        }
+        finally {
+            ClientDisconnected.Invoke(endPoint);
+            stream.Close();
+        }
+
+        return Task.CompletedTask;
+    }
+}
--- EasyTcp/Listener/JsonListener.cs
+++ EasyTcp/Listener/JsonListener.cs
@@ -1,5 +1,6 @@
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 namespace EasyTcp;
 
@@ -8,55 +9,25 @@ public class JsonListener {
     public event Action<string> ClientConnected;
     public event Action<string> ClientDisconnected;
 
-    private readonly TcpListener _listener;
     private CancellationTokenSource _cancelTokenSource = new();
 
+    private ByteListener _byteListener;
+
     public JsonListener(string ipAddress, int port) {
-        _listener = new TcpListener(IPAddress.Parse(ipAddress), port);
+        _byteListener = new ByteListener(ipAddress, port);
+        _byteListener.ClientConnected += (endpoint) => { ClientConnected?.Invoke(endpoint); };
+        _byteListener.ClientDisconnected += (endpoint) => { ClientDisconnected?.Invoke(endpoint); };
+        _byteListener.BytesReceived += (bytes) => {
+            string result = Encoding.UTF8.GetString(bytes);
+            JsonReceived?.Invoke(result);
+        };
     }
 
     public void Start() {
-        _listener.Start();
-        _cancelTokenSource = new CancellationTokenSource();
-
-        while (!_cancelTokenSource.IsCancellationRequested) {
-            TcpClient client = _listener.AcceptTcpClient();
-            Task.Run(() => { _ = _HandleClient(client, _cancelTokenSource.Token); }, _cancelTokenSource.Token);
-        }
+        _byteListener.Start();
     }
 
     public void Stop() {
-        _cancelTokenSource.Cancel();
-        _listener.Stop();
-    }
-
-    private Task _HandleClient(TcpClient client, CancellationToken token) {
-        NetworkStream stream = client.GetStream();
-        byte[] buffer = new byte[1024];
-        string endPoint = $"{client.Client.RemoteEndPoint}";
-        ClientConnected?.Invoke(endPoint);
-
-        try {
-            // Console.WriteLine($"{client.Client.RemoteEndPoint} try");
-            while (!token.IsCancellationRequested) {
-                stream.ReadExactly(buffer, 0, 4);
-                int length = BufferReader.ReadInt(buffer, 0);
-                stream.ReadExactly(buffer, 4, length);
-                string json = BufferReader.ReadString(buffer, 4, length);
-                JsonReceived?.Invoke(json);
-            }
-        }
-        catch (EndOfStreamException) {
-            // Console.WriteLine($"{endPoint} stream ended");
-        }
-        catch (Exception e) {
-            // Console.WriteLine($"{endPoint} catch with exception: {e}");
-        }
-        finally {
-            ClientDisconnected.Invoke(endPoint);
-            stream.Close();
-        }
-
-        return Task.CompletedTask;
+        _byteListener.Stop();
     }
 }
